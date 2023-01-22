36e667050376016aaa663c794aca44126e2b3c22
Andres Lopez <profesor.de.ingles@aol.com>
2023-01-22 18:06:49 -0500

    ADDED env vars to hide secret key

--- citationapp/settings.py
+++ citationapp/settings.py
@@ -12,16 +12,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!8u99thr)3#9x9#+w^pnr@*1joif-x6m5x9)el^qf^ho2qiwc3'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
